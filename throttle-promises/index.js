0bc41f0fcda8a2df4fc25f018d9e008212e14bf1
Liob.Liu <liukeyi_300@hotmail.com>
2017-04-04 13:53:22 +0800

    throttle-promises

--- throttle-promises/index.js
+++ throttle-promises/index.js
@@ -1,27 +1,31 @@
 /**
  * Created by liukeyi on 2017/4/4.
  */
-module.exports = function(limit, promiseArray) {
-  const ctx = this;
-  const args = [];
+module.exports = function(limit, promiseAll) {
+  return new Promise(resolve => {
+    const resultArr = [];
+    let promiseNum = 0;
+    let runIndex = 0;
+    runAPromise(runIndex);
 
-  for (let i = 0; i < limit; i ++) {
-    promiseArray[i].call(this, null).then(function() {
-
-    });
-  }
-
-  let thenFunc = function() {};
-
-  function then(func) {
-    thenFunc = func;
-  }
-
-  function singleResolve(result) {
-
-  }
-
-  return {
-    then: then
-  };
+    function runAPromise(index) {
+      if (index < promiseAll.length) {
+        if (promiseNum < limit) {
+          const promise = promiseAll[index];
+          runIndex ++;
+          promiseNum ++;
+          promise().then(function(result) {
+            resultArr[index] = result;
+            promiseNum --;
+            runAPromise(runIndex);
+          });
+          runAPromise(runIndex);
+        }
+      } else {
+        if (promiseNum === 0) {
+          resolve(resultArr);
+        }
+      }
+    }
+  });
 };
